8c4d5d20b61bfae518a550fb8a980631ce194e53
MarkoTunjic <marko.tunjic21@gmail.com>
2022-10-24 22:50:38 +0200

    Add auth0 support

--- projekt1-backend/src/main/java/hr/fer/web2/projekt1/Projekt1Application.java
+++ projekt1-backend/src/main/java/hr/fer/web2/projekt1/Projekt1Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Projekt1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Projekt1Application.class, args);
 	}
